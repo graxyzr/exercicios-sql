78d7162cf2110f0beec4a54c2be7529d3df85239
Greice Pereira <118115939+graxyzr@users.noreply.github.com>
2023-08-17 15:07:36 -0300

    Add files via upload

---
+++ pessoa/pessoa.sql
@@ -0,0 +1,8 @@
+create table pessoa (
+   cpf char(11),
+   nome varchar(100)
+);
+
+insert into pessoa (cpf, nome) values ('12345678901', 'fulano');
+insert into pessoa (cpf, nome) values ('12345678902', 'ciclano');
+insert into pessoa (cpf, nome) values ('12345678903', 'beltrano');
